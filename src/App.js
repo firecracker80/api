dc746ab8e8fd3ef2da50ac0ec57ba05f5eb35629
Yajahira <yari@yvelazquez.com>
2022-06-21 16:42:02 -0400

    app is functioning

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       city: '',
-      cityFind: {},
+      cityFind: null,
       cityMap: '',
       error: false,
       errorMessage: ''
@@ -33,10 +33,12 @@ class App extends React.Component {
     try {
       let url = (`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
       let cityFind = await axios.get(url);
-      let cityMap = await `https://maps.locationiq.com/v3/staticmap?key=pk.579aac8209bc39c58f89e14dcc5a9a2b}&  center=${cityFind.data[0].lat}, ${cityFind.data[0].lon}&zoom=12`;
+      let cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityFind.data[0].lat},${cityFind.data[0].lon}&zoom=12`;
+      // let cityInfoUrl = `${process.env.REACT_APP_SERVER}/city-explorer?city=${this.state.city}`
       this.setState({
         cityFind: cityFind.data[0],
         cityMap: cityMap
+        // cityInfo:
       })
     }
 
@@ -48,10 +50,10 @@ class App extends React.Component {
       })
     }
 
-    console.log(cityFind.data);
   }
 
   render() {
+    console.log(this.state.cityMap);
 
     return (
       <>
@@ -64,21 +66,27 @@ class App extends React.Component {
           </Form.Group>
         </Form>
         {this.state.error}<Alert variant="info">{this.state.errorMessage}</Alert>
-        <>
-        <Card border="info" style={{ width: '18rem' }} id='Card' onSubmit={this.handleSubmit}>
-          <Card.Body>
-            <Card.Title>{cityFind.data[0].display_name}</Card.Title>
-            <Card.Img variant="top" src={this.state.cityMap} />
-            <Card.Text>
-              {cityFind.data[0].lat}
-              {cityFind.data[0].lon}
-            </Card.Text>
-          </Card.Body>
-        </Card></>
-      </>
-    )
+        
+          {
+            this.state.cityFind ?
+            <Card border="info" style={{ width: '18rem' }} id='Card' onSubmit={this.handleSubmit}>
+              <Card.Body>
+                <Card.Title>{this.state.cityFind.display_name}</Card.Title>
+                <Card.Img variant="top" src={this.state.cityMap} />
+                <Card.Text>
+                  Latitude: {this.state.cityFind.lat}
+
+                </Card.Text>
+                <Card.Text>
+                  Longitude: {this.state.cityFind.lon}
+                </Card.Text>
+              </Card.Body>
+            </Card>: <p>Please enter a city.</p>
+          }
+        </>
+        )
   }
 };
 
 
-export default App;
+        export default App;
